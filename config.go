5ac35444a21be8410929459d34840e070ac925dd
kimhi65 <kimhi65@gmail.com>
2019-11-03 10:17:21 +0900

    191103-1017-Kim

---
+++ config.go
@@ -0,0 +1,53 @@
+package abango
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+)
+
+type Config map[string]string
+
+func ReadConfig(filename string) (Config, error) {
+	// init with some bogus data
+	config := Config{
+		"port":     "8888",
+		"password": "abc123",
+		"ip":       "127.0.0.1",
+	}
+	if len(filename) == 0 {
+		return config, nil
+	}
+	file, err := os.Open(filename)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := bufio.NewReader(file)
+
+	for {
+		line, err := reader.ReadString('\n')
+
+		// check if the line has = sign
+		// and process the line. Ignore the rest.
+		if equal := strings.Index(line, "="); equal >= 0 {
+			if key := strings.TrimSpace(line[:equal]); len(key) > 0 {
+				value := ""
+				if len(line) > equal {
+					value = strings.TrimSpace(line[equal+1:])
+				}
+				// assign the config map
+				config[key] = value
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return nil, err
+		}
+	}
+	return config, nil
+}
